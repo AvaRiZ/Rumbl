5c1df6c2c3475e59d54c809a6867865d96f8cf71
Yaji_z <jaynielpernecita@gmail.com>
2026-03-05 16:35:19 +0800

    Added Unit Test for watch_room

--- test/rumbl/watch_along/watch_room_test.exs
+++ test/rumbl/watch_along/watch_room_test.exs
@@ -3,12 +3,11 @@ defmodule Rumbl.WatchAlong.WatchRoomTest do
 
   alias Rumbl.WatchAlong.WatchRoom
 
-  test "changeset requires code, video_id, and host_id" do
+  test "changeset requires code and host_id" do
     changeset = WatchRoom.changeset(%WatchRoom{}, %{})
 
     refute changeset.valid?
     assert "can't be blank" in errors_on(changeset).code
-    assert "can't be blank" in errors_on(changeset).video_id
     assert "can't be blank" in errors_on(changeset).host_id
   end
 end
